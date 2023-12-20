4de658bd24d5a5e79dce02c5360b8fbf77b59fff
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-20 16:58:08 -0600

    sprite loaded and drawn

--- _build/main.cpp
+++ _build/main.cpp
@@ -7,18 +7,28 @@ int main() {
 	//window dimensions;
 	const int WINDOW_WIDTH{512};
 	const int WINDOW_HEIGHT{380};
-	//rectangle dimensions
-	const int WIDTH{ 50 };
-	const int HEIGHT{ 80 };
+	////rectangle dimensions
+	//const int WIDTH{ 50 };
+	//const int HEIGHT{ 80 };
 	bool isInAir{false};
 	// jump velocity
 	const int JUMP_VEL{ -22 };
 
-	int posY{ WINDOW_HEIGHT - HEIGHT };
+	/*int posY{ WINDOW_HEIGHT - HEIGHT };*/
 	int velocity{0};
 
 	//acceleration due to gravity (pixels/frame/frame)
 	const int gravity{ 1 };
+
+	Texture2D scarfy = LoadTexture("textures/scarfy.png");
+	Rectangle scarfyRec;
+	scarfyRec.width = scarfy.width / 6;
+	scarfy.height = scarfy.height;
+	scarfyRec.x = 0;
+	scarfyRec.y = 0;
+	Vector2 scarfyPos;
+	scarfyPos.x = WINDOW_WIDTH / 2 - scarfyRec.width / 2;
+	scarfyPos.y = WINDOW_HEIGHT - scarfyRec.height;
 
 	//initialize the window
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "DASHER");
@@ -29,10 +39,10 @@ int main() {
 		BeginDrawing();
 		ClearBackground(WHITE);
 
-		DrawRectangle(WINDOW_WIDTH / 2, posY, WIDTH, HEIGHT, BLUE);
+		
 
 		//ground check
-		if (posY >= WINDOW_HEIGHT - HEIGHT) {
+		if (scarfyPos.y >= WINDOW_HEIGHT - scarfy.height) {
 			//on the ground
 			velocity = 0;
 			isInAir = false;
@@ -49,10 +59,14 @@ int main() {
 
 
 		//update position
-		posY += velocity;
+		scarfyPos.y += velocity;
+
+		DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
 		//Stop drawing
 		EndDrawing();
 	}
+
+	UnloadTexture(scarfy);
 	CloseWindow();
 }
