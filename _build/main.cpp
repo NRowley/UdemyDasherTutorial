6075aeb1334ef5e27117d7115799f3c26270b573
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-20 13:10:18 -0600

    configured velocity and jump with space key

--- _build/main.cpp
+++ _build/main.cpp
@@ -7,6 +7,14 @@ int main() {
 	//window dimensions;
 	const int WINDOW_WIDTH{512};
 	const int WINDOW_HEIGHT{380};
+	//rectangle dimensions
+	const int WIDTH{ 50 };
+	const int HEIGHT{ 80 };
+
+	int posY{ WINDOW_HEIGHT - HEIGHT };
+	int velocity{0};
+
+
 	//initialize the window
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "DASHER");
 
@@ -15,6 +23,12 @@ int main() {
 		//Start Drawing
 		BeginDrawing();
 		ClearBackground(WHITE);
+
+		DrawRectangle(WINDOW_WIDTH / 2, posY, WIDTH, HEIGHT, BLUE);
+		if (IsKeyPressed(KEY_SPACE)) {
+			velocity -= 10;
+		}
+		posY += velocity;
 
 		//Stop drawing
 		EndDrawing();
