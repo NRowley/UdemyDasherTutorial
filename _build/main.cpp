2258bd10e69ea17e0207851a7c049038619fd10a
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-20 12:30:07 -0600

    Downloaded textures, setup window

--- _build/main.cpp
+++ _build/main.cpp
@@ -4,5 +4,20 @@
 using namespace std;
 
 int main() {
+	//window dimensions;
+	const int WINDOW_WIDTH{512};
+	const int WINDOW_HEIGHT{380};
+	//initialize the window
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "DASHER");
 
+	SetTargetFPS(60);
+	while (!WindowShouldClose()) {
+		//Start Drawing
+		BeginDrawing();
+		ClearBackground(WHITE);
+
+		//Stop drawing
+		EndDrawing();
+	}
+	CloseWindow();
 }
