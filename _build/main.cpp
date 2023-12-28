5f66ddec301960a19884378e0ff8d61377175e73
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-27 21:33:12 -0600

    loaded nebula texture, set up nebula movement, froze anim for jumping

--- _build/main.cpp
+++ _build/main.cpp
@@ -5,23 +5,31 @@ using namespace std;
 
 int main() {
 	//window dimensions;
-	const int WINDOW_WIDTH{512};
-	const int WINDOW_HEIGHT{380};
+	const int WINDOW_WIDTH{ 512 };
+	const int WINDOW_HEIGHT{ 380 };
 	////rectangle dimensions
 	//const int WIDTH{ 50 };
 	//const int HEIGHT{ 80 };
-	bool isInAir{false};
+	bool isInAir{ false };
 	// jump velocity
 	const int JUMP_VEL{ -600 };
 	//initialize the window
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "DASHER");
 
 	/*int posY{ WINDOW_HEIGHT - HEIGHT };*/
-	int velocity{0};
+	int velocity{ 0 };
 
 	//acceleration due to gravity (pixels/s)/s
 	const int gravity{ 1000 };
 
+	//Nebula variables
+	Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+	Rectangle nebRec{ 0.0, 0.0, nebula.width / 8, nebula.height / 8 };
+	Vector2 nebPos{WINDOW_WIDTH, WINDOW_HEIGHT - nebRec.height};
+	//nebula X velocity
+	int nebVel{-600};
+
+	//Scarfy variables
 	Texture2D scarfy = LoadTexture("textures/scarfy.png");
 	Rectangle scarfyRec;
 	scarfyRec.width = scarfy.width / 6;
@@ -62,27 +70,34 @@ int main() {
 			velocity += JUMP_VEL;
 		}
 
+		//update nebula position
+		nebPos.x += nebVel * deltaTime;
 
 		//update position
 		scarfyPos.y += velocity * deltaTime;
-		runningTime += deltaTime;
-		if (runningTime >= UPDATE_TIME) {
-			runningTime = 0.0;
-			//update animation frame
-			scarfyRec.x = frame * scarfyRec.width;
-			frame++;
-			if (frame > 5) {
-				frame = 0;
+		if (!isInAir) {
+			runningTime += deltaTime;
+			if (runningTime >= UPDATE_TIME) {
+				runningTime = 0.0;
+				//update animation frame
+				scarfyRec.x = frame * scarfyRec.width;
+				frame++;
+				if (frame > 5) {
+					frame = 0;
+				}
 			}
 		}
-		
+		//draw nebula
+		DrawTextureRec(nebula, nebRec, nebPos, WHITE);
 
+		//draw scarfy
 		DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
 		//Stop drawing
 		EndDrawing();
 	}
 
+	UnloadTexture(scarfy);
 	UnloadTexture(scarfy);
 	CloseWindow();
 }
